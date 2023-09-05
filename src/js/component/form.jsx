ed6136dbec7c51d1538011c54b20bc20641b9a1b
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-09-05 15:28:34 +0000

    First Commit

---
+++ src/js/component/form.jsx
@@ -0,0 +1,48 @@
+
+import React, { useState } from "react";
+
+const Form =()=>{
+
+  const [todo,SetTodo]=useState('');
+  const [todos,setTodos]=useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevents the form from submitting and refreshing the page
+    if (todo.trim() !== '') {
+      setTodos([...todos, todo]);//spread operator... This syntax is used in the setTodos function to create a new array that includes all the existing todos
+      setTodo(" ");
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+ 
+return (
+<div classname="container">
+  <h1>Todos</h1>
+   
+<form className="TodoForm" onSubmit={handleSubmit}>
+ <input type="text" value={todo} onChange={(e)=> SetTodo(e.target.value)} className="todo-input" placeholder="What is the task today?"/>
+  
+  <button type="submit" className="btn btn-primary">Submit</button>
+
+  </form>
+  <ul className="List">
+        {todos.map((item, index) => (
+          <li key={index}>
+            <label>{item}</label>
+            <button className="btn" onClick={() => handleDelete(index)}>
+              <i className="fa fa-trash" aria-hidden="true"></i>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+        
+
+export default Form;
