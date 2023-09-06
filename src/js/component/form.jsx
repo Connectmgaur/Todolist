07d56164e2b73895e38c01778d6a55e5abe41e3e
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-09-06 18:59:06 +0000

    To do list version 1

--- src/js/component/form.jsx
+++ src/js/component/form.jsx
@@ -3,14 +3,15 @@ import React, { useState } from "react";
 
 const Form =()=>{
 
-  const [todo,SetTodo]=useState('');
+  const [todo,setTodo]=useState('');
   const [todos,setTodos]=useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault(); // Prevents the form from submitting and refreshing the page
     if (todo.trim() !== '') {
       setTodos([...todos, todo]);//spread operator... This syntax is used in the setTodos function to create a new array that includes all the existing todos
-      setTodo(" ");
+      setTodo("");
+
     }
   };
 
@@ -21,11 +22,11 @@ const Form =()=>{
 
  
 return (
-<div classname="container">
+<div classname="container d-flex">
   <h1>Todos</h1>
    
 <form className="TodoForm" onSubmit={handleSubmit}>
- <input type="text" value={todo} onChange={(e)=> SetTodo(e.target.value)} className="todo-input" placeholder="What is the task today?"/>
+ <input type="text" value={todo} onChange={(e)=> setTodo(e.target.value)} className="todo-input" placeholder="What is the task today?"/>
   
   <button type="submit" className="btn btn-primary">Submit</button>
 
@@ -34,12 +35,15 @@ return (
         {todos.map((item, index) => (
           <li key={index}>
             <label>{item}</label>
-            <button className="btn" onClick={() => handleDelete(index)}>
+            <button className="btn-delete" onClick={() => handleDelete(index)}>
               <i className="fa fa-trash" aria-hidden="true"></i>
             </button>
           </li>
+          
         ))}
+        
       </ul>
+      <div>{todos.length}</div>
     </div>
   );
 };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Form from "./form";
 
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
 const Home = () => {
